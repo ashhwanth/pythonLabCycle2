1460ccbc7fa992096ae721bf519c323d44b5717d
Aswanth Haridas <57171141+ashhwanth@users.noreply.github.com>
2022-12-12 00:08:04 +0530

    Update starPyramidCycle2.py

--- starPyramidCycle2.py
+++ starPyramidCycle2.py
@@ -1,9 +1,11 @@
-row=int(input("Enter the number of rows"))
-for i in range(row):
-    for j in range(i+1):
-        print("*",end=" ")
-    print("\n")
-for k in range(row,1,-1):
-    for h in range(k-1):
-        print("*",end=" ")
-    print("\n")
+def starpyramid():
+    row=int(input("Enter the number of rows"))
+    for i in range(row):
+        for j in range(i+1):
+            print("*",end=" ")
+        print("\n")
+    for k in range(row,1,-1):
+        for h in range(k-1):
+            print("*",end=" ")
+        print("\n")
+starpyramid()
